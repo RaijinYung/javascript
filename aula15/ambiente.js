b457186e7c57a6ee6891acddab6cd6ccb17fc7ab
RaijinYung <123278773+RaijinYung@users.noreply.github.com>
2023-03-29 21:01:55 -0300

    up

---
+++ aula15/ambiente.js
@@ -0,0 +1,6 @@
+let num = [5, 8, 2,]
+num[3] = 6
+num.push(7)
+
+
+console.log(`Nosso vetor é o ${num}`)
---
+++ aula15/vetornatela.js
@@ -0,0 +1,15 @@
+let valores = [8, 1, 7, 4, 2, 9]
+
+/*
+for (let pos = 0; pos < valores.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+
+} */
+
+
+// de forma simplificada 
+//for(let pos  in valores) {
+   // console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+//}
+let pos = valores.indexOf(7)
+console.log(`valor ${valores} esta na posição ${pos}`)
